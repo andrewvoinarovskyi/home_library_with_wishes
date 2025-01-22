0bcf2cbd57cd49b8e53b8d91eb27bc79dc5d2e05
andrii <avoinarovskiy@gmail.com>
2025-01-22 15:16:38 +0200

    add improvements to styles

--- frontend/src/components/ItemCard/ItemCard.jsx
+++ frontend/src/components/ItemCard/ItemCard.jsx
@@ -7,7 +7,7 @@ const ItemCard = ({ item }) => {
     <div className='card'>
       <div className='pic'>isdnvoi siwn evroivb oiwer nb</div>
       <h3 className="italic">{item.name}</h3>
-      <p>Категорія: {categories[item.category]}</p>
+      <p className='no-wrap'>Категорія: {categories[item.category]}</p>
       <p>К-ть гравців: 2-6</p>
       <p>Рейтинг: {item.priority || 'None'}</p>
     </div>
