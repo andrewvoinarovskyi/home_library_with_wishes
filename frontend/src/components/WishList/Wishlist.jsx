2f40528e3f69a9de949ed51e24fdb828f80b751a
andrii <avoinarovskiy@gmail.com>
2025-01-22 16:28:57 +0200

    install rspec and eslint

--- frontend/src/components/WishList/Wishlist.jsx
+++ frontend/src/components/WishList/Wishlist.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { fetchWishlist } from '../../api/collectibleItems.js';
 
 const Wishlist = () => {
