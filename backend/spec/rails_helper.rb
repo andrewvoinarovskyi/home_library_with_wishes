e6875fdc72370ceaa6faaa94e3f4cccb2898e463
andrii <avoinarovskiy@gmail.com>
2025-01-29 19:58:44 +0200

    add specs for Collectible Items controller with additional configurations

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -69,4 +69,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # include supporting methods for testing controllers
+  config.include RequestSpecHelper, type: :controller
 end
