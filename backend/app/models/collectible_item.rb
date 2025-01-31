af35692fa3cbddbc67da13f26c0474dae7016e06
andrii <avoinarovskiy@gmail.com>
2025-01-31 15:22:25 +0200

    add collectible item controller specs
    add error handlers for application controller

--- backend/app/models/collectible_item.rb
+++ backend/app/models/collectible_item.rb
@@ -1,6 +1,7 @@
 class CollectibleItem < ApplicationRecord
     validates :name, presence: true
     validates :category, presence: true
+    validates :status, presence: true
   
     scope :books, -> { where(category: 'book') }
     scope :games, -> { where(category: 'game') }
